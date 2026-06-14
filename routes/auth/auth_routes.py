f914046eac79afc1e975f4da70c5d2121dea2f9e
Shashank Yadav <shashankyadav3054@gmail.com>
2026-06-14 16:49:06 +0530

    Login and dashboard module completed

--- routes/auth/auth_routes.py
+++ routes/auth/auth_routes.py
@@ -1,12 +1,28 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from database.models import User
 from database.db import db
 from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(
+            username=username,
+            password=password
+        ).first()
+
+        if user:
+            return redirect(url_for("auth.dashboard"))
+
+        return "Invalid Username or Password"
+
     return render_template("login.html")
 
 @auth.route("/register", methods=["GET", "POST"])
@@ -28,3 +44,8 @@ def register():
         return "User Registered Successfully"
 
     return render_template("register.html")
+
+
+@auth.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
